f0a4b868ae640857528cab3c2fa47710088a28d6
sherlok <http://github.com/sherlok>
2009-12-08 07:59:16 +0800

    forgot to add buildit script -_-

--- Machine.py
+++ Machine.py
@@ -1,24 +1,36 @@
 from buildit import *
 
-libs = ['sfml-graphics', 'sfml-window', 'sfml-system']
+libs = [
+	'sfml-graphics',
+	'sfml-window',
+	'sfml-system'
+]
+
+compiler_flags = [
+	'-Wall',
+	'-Wextra',
+	'-g'
+]
 
 #build for Unix
 machine = Unix("Machine")
-
-#Set the compiler
 machine.compiler = CXX()
 
 #add our source directorys
 machine.add('src', True)
 
+#add some compiler flags
+machine.add_flag(compiler_flags)
+
 #libraries
 machine.add_library(libs)
-machine.build_directory='bin'
+machine.build_directory = 'bin'
+
+# Copy stuff from the data folder
+data = Content()
+data.add('data', True)
+data.build_directory = machine.build_directory
 
 #buildit
-machine.run();
-
-#post build steps
-data = Content()
-data.add('data')
-data.build_directory = machine.build_directory
+machine.run()
+data.run()
